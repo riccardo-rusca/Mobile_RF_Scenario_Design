2034d43c57860a65963a7b236090cb4d46928d04
Francesco Raviglione <francescorav.es483@gmail.com>
2023-05-11 10:17:30 +0200

    Extend README.md and code comments
    This commit provides an updated README.md with further
    information (especially on the input format for the dataset)
    and extended code comments in the generic code.
    It also introduces few small bug fixes.

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 If you use the content of this repository, please reference the following paper: 
 
-> R. Rusca, F. Raviglione, C. Casetti, P. Giaccone f. Restuccia, "Mobile RF Scenario Design for Massive-Scale Wireless Channel Emulators," pre-print version. [URL](https://hdl.handle.net/11583/2977750) [BibTeX](/cite.bib)
+> R. Rusca, F. Raviglione, C. Casetti, P. Giaccone, F. Restuccia, "Mobile RF Scenario Design for Massive-Scale Wireless Channel Emulators," pre-print version. [URL](https://hdl.handle.net/11583/2977750) [BibTeX](/cite.bib)
 
 ### Files Overview
 * [*raytrace_mobile.m*](/raytrace_mobile.m) &rarr; contains the main code with the dataset import, channel matrix initialization and computation of others variables needed. For the Ray-tracing and Clustering step for vehicle and for each timestamp is called the raytrace_fcn inside the *raytrace_fcn.m* file.
 * [*raytrace_fcn.m*](/raytrace_fcn.m) &rarr; contains the code for the ray-tracing and clustering.
+
+### Expected input format
+The code inside [*raytrace_mobile.m*](/raytrace_mobile.m) expects a specific input format for the dataset. You can either read a dataset with a different format and convert it inside MATLAB, or directly provide the dataset with the required format.
+This format is a CSV file with one line for each vehicle and each timestamp, and *at least* the following fields:
+- *agent_id* &rarr; ID of the vehicle the row refers to
+- *timeStamp_posix* &rarr; Timestamp with respect to a well-defined reference, in seconds, and with decimal digits to reprents milliseconds
+- *latitude_deg* &rarr; Latitude in degrees for vehicle *agent_id* at time *timeStamp_posix*
+- *longitude_deg* &rarr; Longitude in degrees for vehicle *agent_id* at time *timeStamp_posix*
+
+### Input required in the generic code
+If you want to use the generic code inside [*raytrace_mobile.m*](/raytrace_mobile.m) (i.e., not the one for the already configured scenario with the *SAMARCANDA* dataset), before you can launch the code and generate your own scenario, you **need** to properly customize the code, inserting at least the required parameters/code in the line followed by a comment with *{INPUT}*.
 
 ### SAMARCANDA Mobility Scenario
 Inside **SAMARCANDA_Scenario** folder you can find all the files needed to create a *Mobile RF Scenario* starting from the *SAMARCANDA* dataset available [here](https://github.com/francescoraves483/ms-van3t-CAM2CEM/raw/master/src/gps-tc/examples/GPS-Traces-Sample/SAMARCANDA_dataset.csv).  
@@ -19,7 +30,7 @@ More in detail:
 
 ### Contacts
 * Riccardo Rusca [riccardo.rusca@polito.it]
-* Francesco Raviglione [francesco.raviglione@polito.it]
+* Francesco Raviglione [francescorav.es483@gmail.com] [francesco.raviglione@polito.it]
 * Claudio Casetti [claudio.casetti@polito.it]
 * Paolo Giaccone [paolo.giaccone@polito.it]
 * Francesco Restuccia [f.restuccia@northeastern.edu]
--- raytrace_mobile.m
+++ raytrace_mobile.m
@@ -6,13 +6,17 @@ clc
 %%% PRE-PROCESSING
 
 %% Final name for the .mat file with the channel matrix
-FINAL_FILENAME="RF_scenario.mat"; % Insert here the final file name
+FINAL_FILENAME="RF_scenario.mat"; % {INPUT} Insert here the final file name
 
 %% Selected communication frequency for the scenario
-COMM_FREQUENCY=1e9; % Should be specified in [Hz]
+COMM_FREQUENCY=1e9; % {INPUT} Should be specified in [Hz]
 
 %% Parse data and define area of interest
-filename = 'dataset.csv'; % Insert here the dataset file name
+filename = 'dataset.csv'; % {INPUT} Insert here the dataset file name
+% {INPUT} Define here and filter here the data to keep only traces inside the area of interest
+
+% {INPUT} Convert here the dataset to match the required format (see
+% README.md)
 
 format long g
 DATASET = readtable(filename);
@@ -28,22 +32,23 @@ for i=1:num_agents
     DATASET_agents{i}=DATASET(DATASET.agent_id==i,:); % Each cell of this cell array contains a sub-table related to a specific vehicle
     DATASET_base_ts{i}=DATASET_agents{i}(1,:).timeStamp_posix; % Cell array of initial timestamps (to compute relative timestampts)
     DATASET_agents{i}.diffTstamp=DATASET_agents{i}.timeStamp_posix-DATASET_base_ts{i}; % Thanks to cell arrays, we can quickly add the relative timestamps to DATASET
-    DATASET_num_points_per_agent(i)=length(DATASET_agents{i}.diffTstamp); % Each value in this array contains the number of trace points (one every around 100 ms) for each vehicle
+    DATASET_num_points_per_agent(i)=length(DATASET_agents{i}.diffTstamp); % Each value in this array contains the number of trace points (one every around x ms, depending on the dataset) for each vehicle
 end
 DATASET_nppa_min = min(DATASET_num_points_per_agent); % Minimum number of points available for each vehicle/agent
 
 %% Fixed BS/RSU coordinates
-antenna_lat = 44.88338;
-antenna_long = 7.33152;
+antenna_lat = 44.88338; % {INPUT} Insert here the latitude of your RSU/BS
+antenna_long = 7.33152; % {INPUT} Insert here the longitude of your RSU/BS
 
 
 %% Initialization of the chMatrix variable
-num_antennas=1;
+num_antennas=1; % This code considers for the time being one centralized RSU/BS
 total_nodes=num_agents+num_antennas;
 
-new_matrix_dimension = 5000; % Insert here number of timestamps
+new_matrix_dimension = 5000; % {INPUT} Insert here the total number of timestamps to be included in the scenario
 chMatrix = cell(total_nodes,total_nodes,new_matrix_dimension);
 
+% Initializing the channel matrix with empty values
 for i=1:total_nodes
     for j=1:total_nodes
         for k=1:new_matrix_dimension
@@ -58,27 +63,26 @@ for i=1:total_nodes
     end
 end
 
-
 %%% RAY-TRACING and CLUSTERING
 if ~isfile(FINAL_FILENAME)
     for i=1:DATASET_nppa_min
 		for x=1:length(real_agents)
 			ag=real_agents(x);
-			else
-				curr_lat=DATASET_agents{ag}(i,:).latitude_deg;
-				curr_lon=DATASET_agents{ag}(i,:).longitude_deg;
-				% Call raytrace functtion inside raytrace_fnc.m
-				[tap_gains,tap_delays]=raytrace_fcn(curr_lat,curr_lon,1.5,23,199.53*1e-3,COMM_FREQUENCY,44.88338,7.33152,30);
-   
-				chMatrix{x,total_nodes,i}.iq=tap_gains(tap_gains~=0)';
-				chMatrix{x,total_nodes,i}.delay=tap_delays(find(tap_gains~=0))';
+            % Get the latitude and longitude of the vehicle at
+            % the current timestamp
+			curr_lat=DATASET_agents{ag}(i,:).latitude_deg;
+			curr_lon=DATASET_agents{ag}(i,:).longitude_deg;
+			% Call the raytrace function inside raytrace_fcn.m
+			[tap_gains,tap_delays]=raytrace_fcn(curr_lat,curr_lon,1.5,23,199.53*1e-3,COMM_FREQUENCY,antenna_lat,antenna_long,30);
 
-				% The channel matrix should be duplex (i.e., bi-directional, vehicles->antenna and antenna->vehicles)
-				chMatrix{total_nodes,x,i}.iq=chMatrix{x,total_nodes,i}.iq;
-				chMatrix{total_nodes,x,i}.delay=chMatrix{x,total_nodes,i}.delay;
-			end
+			chMatrix{x,total_nodes,i}.iq=tap_gains(tap_gains~=0)';
+			chMatrix{x,total_nodes,i}.delay=tap_delays(find(tap_gains~=0))';
+
+			% The channel matrix should be duplex (i.e., bi-directional, vehicles->antenna and antenna->vehicles)
+			chMatrix{total_nodes,x,i}.iq=chMatrix{x,total_nodes,i}.iq;
+			chMatrix{total_nodes,x,i}.delay=chMatrix{x,total_nodes,i}.delay;
 		end
-	end
+    end
 end
 
     save("chMatrix_data.mat","chMatrix")
@@ -87,12 +91,12 @@ else
 end
 
 
-%%% Computation of others variables needed
+%%% Computation of others needed variables
 
 %% Array of timestamps
 timestamps = zeros(1,new_matrix_dimension); 
 
-% How to manage different timestamps for different vehicles?
+% How to manage slightly different timestamps for different vehicles?
 % Workaround: we average them between all the agents/vehicles at each step
 for ts=1:SAMARCANDA_nppa_min
     timestamps_agents=zeros(1,num_agents);
@@ -108,9 +112,9 @@ for ts=1:new_matrix_dimension
     timestamps(ts) = timestamps(ts) - t;
 end
 
-%% Array of ccordinates
+%% Array of coordinates
 coordinates = cell(total_nodes,3,new_matrix_dimension);
-FIXED_ALTITUDE_METERS_ASL=376; % Insert here the altitude
+FIXED_ALTITUDE_METERS_ASL=376; % {INPUT} Insert here the altitude (we suppose here a fixed altitude, for the time being), in [m]
 
 for ts=1:SAMARCANDA_nppa_min
 	for x=1:num_agents
@@ -118,7 +122,7 @@ for ts=1:SAMARCANDA_nppa_min
 		curr_lat=SAMARCANDA_agents{ag}(ts,:).latitude_deg;
 		curr_lon=SAMARCANDA_agents{ag}(ts,:).longitude_deg;
 	
-		% Convert to cartesian coordinates
+		% Convert from geodetic to cartesian ECEF coordinates
 		[curr_x,curr_y,curr_z]=geodetic2ecef(wgs84Ellipsoid('meter'),curr_lat,curr_lon,FIXED_ALTITUDE_METERS_ASL);
 	
 		coordinates{x,1,ts}=curr_x;
@@ -136,9 +140,9 @@ for ts=1:SAMARCANDA_nppa_min
 end
 
 
-%% Setting as origin the center of the 1 km^2 area
-origin.lat=44.883595;
-origin.lon=7.330495;
+%% Setting as origin the center of the area of interest
+origin.lat=44.883595; % {INPUT} Insert here the latitude of the center of the area
+origin.lon=7.330495; % {INPUT} Insert here the longitude of the center of the area
 
 if ~isfile(FINAL_FILENAME)
     save(FINAL_FILENAME,"chMatrix","timestamps","coordinates","origin");
